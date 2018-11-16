30692045b38d673d9658b58619cb9ca33645febd
John Ericson <git@JohnEricson.me>
2018-11-16 05:13:58 -0500

    Bump reflex platform and make a thunk

--- default.nix
+++ default.nix
@@ -1,10 +1,5 @@
 let
-  reflex-platform = import ((import <nixpkgs> {}).fetchFromGitHub {
-    owner  = "reflex-frp";
-    repo   = "reflex-platform";
-    rev    = "1482d393669711a8a528dab2ff0f174d3d768ce2";
-    sha256 = "0qpa5s13hplkfdxihk9gfxcz0pv0si2ghghskg3rrf7f7xl5r67f";
-  }) {};
+  reflex-platform = import ./dep/reflex-platform {};
 in
 reflex-platform.project ({ pkgs, ... }: {
   packages = {
